f95caa8ede8cbecc62859e59de1117e64b5fc055
ChrisRogerpm <ChristianRogerpm@gmail.com>
2018-05-29 01:35:30 -0500

    Add WebServices
    Add WebServices

--- app/Http/Controllers/Api/WebServiceController.php
+++ app/Http/Controllers/Api/WebServiceController.php
@@ -21,11 +21,39 @@ class WebServiceController extends Controller
         if (Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
             $user = Auth::user();
             $result = "success";
-//            return response()->json($result);
             echo 'success';
-            //return Response::json($user);
         } else {
             echo "no";
         }
+    }
+
+    public function ListarEventoFiesta()
+    {
+        $eventos_fiesta = Evento::where('categoria_id', 1)->get();
+        return Response::json($eventos_fiesta);
+    }
+
+    public function ListarEventoMusica()
+    {
+        $eventos_musica = Evento::where('categoria_id', 2)->get();
+        return Response::json($eventos_musica);
+    }
+
+    public function ListarEventoGastronomia()
+    {
+        $eventos_gastronomia = Evento::where('categoria_id', 3)->get();
+        return Response::json($eventos_gastronomia);
+    }
+
+    public function ListarEventoDeporte()
+    {
+        $eventos_deporte = Evento::where('categoria_id', 4)->get();
+        return Response::json($eventos_deporte);
+    }
+
+    public function ListarEventoOtro()
+    {
+        $eventos_otro = Evento::where('categoria_id', 6)->get();
+        return Response::json($eventos_otro);
     }
 }
